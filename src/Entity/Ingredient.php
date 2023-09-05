2d9efcfc3e4b147ad5012f48563beadd049bd9f1
Richard Phonthibsvads <richards@MacBook-Pro-de-Richard.local>
2023-09-05 10:37:14 +0200

    add fixtures with Faker php

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -3,7 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\IngredientRepository;
+// use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
 class Ingredient
@@ -14,13 +16,28 @@ class Ingredient
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 50)]
     private ?string $name = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: 'float')]
+    #[Assert\NotNull()]
+    #[Assert\Positive()]
+    #[Assert\LessThan(200)]
     private ?float $price = null;
-
-    #[ORM\Column]
+    
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt = null;
+
+    /**
+     * Constructor
+     * createdAt de type DateTiemImmutable
+     */
+    public function __construct()
+    {
+        return $this->createdAt = new \DateTimeImmutable();      
+    }
 
     public function getId(): ?int
     {
